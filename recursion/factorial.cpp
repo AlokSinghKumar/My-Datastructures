4426870edbf3db85ec62f2741060f7d340b04c38
AlokSinghKumar <aloksinghkumar23@gmail.com>
2023-06-03 22:09:31 +0530

    recursion

---
+++ recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void imp(){
+
+  #ifndef ONLINE_JUDGE
+      freopen("input.txt","r",stdin);
+      freopen("output.txt","w",stdout);
+  #endif
+}
+
+int countIf (int elem) {
+    return elem % 2 == 0;
+}
+
+
+int main () {
+    imp ();
