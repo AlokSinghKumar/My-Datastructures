73036791bec9e116f8c42bd61f3f9ce88f916189
AlokSinghKumar <aloksinghkumar23@gmail.com>
2023-06-03 22:16:52 +0530

    factorial

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -4,8 +4,8 @@ using namespace std;
 void imp(){
 
   #ifndef ONLINE_JUDGE
-      freopen("input.txt","r",stdin);
-      freopen("output.txt","w",stdout);
+      freopen("../input.txt","r",stdin);
+      freopen("../output.txt","w",stdout);
   #endif
 }
 
@@ -13,6 +13,22 @@ int countIf (int elem) {
     return elem % 2 == 0;
 }
 
+int factorial (int n) {
+    if (n <= 1)
+        return 1;
+
+    return n * factorial (n - 1);
+}
 
 int main () {
     imp ();
+
+    int n;
+    cin >> n;
+
+    int ans = factorial (n);
+
+    cout << ans;
+
+    return 0;
+}
