f8643632a58e930537ffe005f4f0facc327884c4
AlokSinghKumar <aloksinghkumar23@gmail.com>
2023-06-04 18:34:47 +0530

    latest

--- temp.cpp
+++ temp.cpp
@@ -10,21 +10,39 @@ void imp(){
   #endif
 }
 
-void countN (int n) {
-    if (n > 1)
-        countN (n - 1);
-
-    cout << n << " ";
-}
-
-
 int main () {
     imp ();
 
-    int n;
-    cin >> n;
+    vector <long long> arr;
+    int n, k;
 
-    countN (n);
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        long long tmp;
+        cin >> tmp;
+
+        arr.push_back (tmp);
+    }
+
+    vector<long long>::iterator start = arr.begin (), end;
+    int cycle = ceil ((float)n/(float)k);
+
+    if (k > 1) {
+        for (int i = 1 ; i <= cycle; i++ ) {
+            end = arr.begin () + (i * k) > arr.end () ? arr.end () : arr.begin () + (i * k);
+
+            reverse (start, end);
+
+            cout << endl;
+            start = end;
+        }
+    }
+
+
+    for (auto itr : arr) {
+        cout << itr << " ";
+    }
 
     return 0;
 } 
