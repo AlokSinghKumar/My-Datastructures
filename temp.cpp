a388614d31ad1c8ec0934a4b267a5ea3c8bc6c8f
Alok Kumar Singh <aloksinghkumar23@gmail.com>
2023-11-14 18:08:08 +0530

    added gitignore

--- temp.cpp
+++ temp.cpp
@@ -12,37 +12,15 @@ void imp(){
 
 int main () {
     imp ();
+    vector<int> arr;
+    int n, s;
 
-    vector <long long> arr;
-    int n, k;
-
-    cin >> n >> k;
+    cin >> n >> s;
 
     for (int i = 0; i < n; i++) {
-        long long tmp;
+        int tmp;
         cin >> tmp;
 
         arr.push_back (tmp);
     }
-
-    vector<long long>::iterator start = arr.begin (), end;
-    int cycle = ceil ((float)n/(float)k);
-
-    if (k > 1) {
-        for (int i = 1 ; i <= cycle; i++ ) {
-            end = arr.begin () + (i * k) > arr.end () ? arr.end () : arr.begin () + (i * k);
-
-            reverse (start, end);
-
-            cout << endl;
-            start = end;
-        }
-    }
-
-
-    for (auto itr : arr) {
-        cout << itr << " ";
-    }
-
-    return 0;
-} 
+}
